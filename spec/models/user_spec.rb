44075ec510bccefd13237d7b206f10d523b8ac56
Dario Barrionuevo <dario@insignia4u.com>
2013-06-14 17:04:27 -0300

    User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
-
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let!(:own_idea) { create(:idea, user_id: user.id) }
+  let!(:idea) { create(:idea) }
+
+  describe "Validations" do
+    it{ should validate_presence_of :uid }
+    it{ should validate_presence_of :name }
+    it{ should validate_presence_of :email }
+  end
+
+  describe "#author_of?" do
+    it { expect(user.author_of? own_idea).to be_true }
+    it { expect(user.author_of? idea).to be_false }
+  end
+
+  describe "#has_kicked_ideas" do
+
+  end
 end
